15657cd1b457ba24c5d2436370c0f83b1544af67
Nabil Fadhiilah <nabil.10518027@mahasiswa.unikom.ac.id>
2022-08-04 18:32:31 +0700

    admin dashboard

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pengembangan;
+use App\Models\ReservasiPaketWisata;
 use Illuminate\Http\Request;
+use App\Models\ReservasiWisata;
 
 class AdminDashboardController extends Controller
 {
@@ -10,6 +13,9 @@ class AdminDashboardController extends Controller
     public function index()
     {
         # code...
-        return view('pages.admin.index');
+        $wisata = ReservasiWisata::where('bukti_reservasi', '!=', null)->where('tgl_verifikasi', '=', null)->count();
+        $paket = ReservasiPaketWisata::where('bukti_reservasi', '!=', null)->where('tgl_verifikasi', '=', null)->count();
+        $invest = Pengembangan::where('bukti_pembayaran', '!=', null)->where('tgl_verifikasi', '=', null)->count();
+        return view('pages.admin.index', ['wisata' => $wisata, 'paket' => $paket, 'invest' => $invest]);
     }
 }
--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -1,4 +1,90 @@
 @extends('pages.admin.layouts.app')
 @section('content')
-    Hello
+    <div class="page-heading">
+        <h3>Hallo Admin!</h3>
+    </div>
+    <div class="page-content">
+        <section class="row">
+            <div class="col-12 col-lg-12">
+                <div class="row">
+                    <div class="col-6 col-lg-4 col-md-6">
+                        <div class="card">
+                            <div class="card-body px-3 py-4-5">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="stats-icon purple">
+                                            <i class="iconly-boldDiscovery"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <h6 class="text-muted font-semibold">Verifikasi Pembayaran Wisata</h6>
+                                        <h6 class="font-extrabold mb-0"><a class="text-decoration-underline"
+                                                href="{{ route('admin.verifikasi-wisata.index') }}">{{ $wisata }}
+                                                Transaksi</a></h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-lg-4 col-md-6">
+                        <div class="card">
+                            <div class="card-body px-3 py-4-5">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="stats-icon blue">
+                                            <i class="iconly-boldProfile"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <h6 class="text-muted font-semibold">Verifikasi Pembayaran Paket</h6>
+                                        <h6 class="font-extrabold mb-0">
+                                            <a class="text-decoration-underline"
+                                                href="{{ route('admin.verifikasi-paket.index') }}">{{ $paket }}
+                                                Transaksi</a>
+                                        </h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 col-lg-4 col-md-6">
+                        <div class="card">
+                            <div class="card-body px-3 py-4-5">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="stats-icon green">
+                                            <i class="iconly-boldInfo-Circle"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <h6 class="text-muted font-semibold">Verifikasi Pembayaran Pengembangan</h6>
+                                        <h6 class="font-extrabold mb-0"> <a class="text-decoration-underline"
+                                                href="{{ route('admin.verifikasi-pengembangan.index') }}">{{ $invest }}
+                                                Transaksi</a></h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- <div class="col-6 col-lg-3 col-md-6">
+                        <div class="card">
+                            <div class="card-body px-3 py-4-5">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="stats-icon red">
+                                            <i class="iconly-boldBookmark"></i>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <h6 class="text-muted font-semibold">Saved Post</h6>
+                                        <h6 class="font-extrabold mb-0">112</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div> --}}
+                </div>
+            </div>
+        </section>
+    </div>
 @endsection
